9d42588852ebadb05de9f54d8a23a40a2e64b973
Florian Duraffourg <florian.duraffourg@bestmile.com>
2018-09-08 14:41:14 +0200

    Send extension handshake

--- src/Main.hs
+++ src/Main.hs
@@ -10,17 +10,27 @@ main = do
   putStrLn "Socket created"
   connect sock sockAddr
   putStrLn "Socket connected"
-  send sock initialMessage
-  putStrLn "Initial message sent"
-  response <- recv sock 1024
-  putStrLn $ show response
+  send sock handshakeMessage
+  putStrLn "Handshake message sent"
+  send sock extensionHandshakeMessage
+  putStrLn "Extension handshake sent"
+  handShakeResponse <- recv sock 68
+  putStrLn $ show handShakeResponse
 
 sockAddr = SockAddrInet 6881 (tupleToHostAddress (5,39,91,197))
 
-initialMessage = BS.pack
+handshakeMessage = BS.pack
   [19 -- Lenght prefix
   , 66,105,116,84,111,114,114,101,110,116,32,112,114,111,116,111,99,111,108 -- BitTorrent protocol
-  ,0,0,0,0,0,0,0,0
+  ,0,0,16,0,0,0,0,0 -- Indicate that we support extensions
   , 254,10,72,13,167,152,42,193,185,141,126,15,9,91,5,223,56,175,46,130 -- InfoHash of Chasing Ice
   ,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42 -- NodeID
+  ]
+
+
+extensionHandshakeMessage = BS.pack
+  [0,0,0,9 -- Length of the message
+  ,20 -- Type: extension
+  ,0 -- Extension type: handshake
+  ,100,49,58,109,100,101,101 -- d1:mdee This is an empty 'm' dictionary
   ]
