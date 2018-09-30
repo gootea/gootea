5245c4b1429a6c6d74a9fd7656965a28c4038fce
Florian Duraffourg <florian.duraffourg@bestmile.com>
2018-09-30 19:22:09 +0200

    Add Collector library

---
+++ CollectorSpec/CollectorSpec.hs
@@ -0,0 +1,52 @@
+module CollectorSpec where
+
+import Collector.Collector
+import Data.Maybe
+import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+
+import Debug.Trace
+
+spec :: Spec
+spec = do
+  describe "Collector" $ do
+    prop
+      "let object go through only once at the appropriate count"
+      propFilterThroughCollection
+
+-------------------------
+-- Arbitrary Instances --
+-------------------------
+newtype Object =
+  Object Int
+  deriving (Show, Eq)
+
+instance Ord Object where
+  compare (Object a) (Object b) = compare a b
+
+instance Arbitrary Object where
+  arbitrary = Object <$> arbitrary
+
+newtype MinSeenCount =
+  MinSeenCount Int
+  deriving (Eq, Show)
+
+instance Arbitrary MinSeenCount where
+  arbitrary = (MinSeenCount . (+ 1) . abs) <$> arbitrary
+
+-------------------
+-- Test Commands --
+-------------------
+propFilterThroughCollection :: MinSeenCount -> Object -> Bool
+propFilterThroughCollection (MinSeenCount minSeenCount) obj =
+  (all id . traceShowId . fmap snd . repeatFilterThrough . newCollection)
+    minSeenCount
+  where
+    repeatFilterThrough :: Collection Object -> [(Collection Object, Bool)]
+    repeatFilterThrough c =
+      scanl doFilterThrough (c, True) [0 .. minSeenCount + 10]
+    doFilterThrough (c, _) count =
+      (isResponseCorrect count) <$> filterThroughCollection c obj
+    isResponseCorrect count r =
+      (isJust (traceShowId r)) == (count == minSeenCount)
